5d2177e70e1ac0fc05b79a74906cacd6bfbaa0a9
Muthu Annamalai Venkatachalam <muthuannamalai2002@gmail.com>
2021-07-01 19:51:52 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+$server_name='localhost';
+$user_name='root';
+$pass_word='';
+$db='banking_site';
+
+$conn = mysqli_connect($server_name,$user_name,$pass_word,$db);
+
+
+
+// if(!$conn){
+//     die("Unable to connect");
+// }
+// else{
+//     echo 'Connected';
+// }
+
+?>
+
